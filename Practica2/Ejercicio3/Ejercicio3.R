bfa77d9dd6080e733f40ac9926de082e2c587ceb
konrad <kbtch95@gmail.com>
2018-06-24 03:24:07 -0500

    finalizado Practica2 ejercicio3 version 13.1

--- Practica2/Ejercicio3/Ejercicio3.R
+++ Practica2/Ejercicio3/Ejercicio3.R
@@ -1,15 +1,9 @@
----
-title: 'Practica2 : Ejercicio 3'
-author: "konrad Trejo"
-date: "30 de mayo de 2018"
-output: pdf_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
+#title: 'Practica2 : Pregunta 3'
+#author: "konrad Trejo"
+#date: "30 de mayo de 2018"
+#output: pdf_document
 #### Item a) Declaramos los vectores vec1 y vec2
-```{r}
+
 
 vec1 <-c(2,1,2,3,2,1,0)
 vec2 <-c(3,8,2,2,0,0,0)
@@ -23,25 +17,22 @@ if(vec1[1]>=2 && vec2[1]>=2) {cat("Imprime el resultado\n")}
 if(!is.na(vec2[3])) {cat("Imprime el resultado \n")}
 help(is.na)
 
-```
-#### En los casos 1,2,4 se cumple que imprime los resultados debido al cumplimineto de los condicionales
-#### Item b) Usando los vectores vec1 y vec2 anteriores imprime el producto si la suma de los vectores es mayor que 3 sino imprime solo la suma de vectores
-```{r}
+#### En los casos 1,2 ,4 se cumple que impirme los resultados debido al cumplimineto de los condicionales
+
+#### Item b)  Usando los vectores vec1 y vec2 anteriores imprime el producto si la suma de los vectores es mayor que 3 sino imprime solo la suma de vectores
 
 for(j in 1:length(vec2)){
- if(vec1[j] + vec2[j] > 3){
-     cat("la suma es: ", vec1[j]+vec2[j], "\n")
-     
+  if(vec1[j] + vec2[j] > 3){
+    cat("la suma es: ", vec1[j]+vec2[j], "\n")
+    
   }else{
     cat("El producto es: ", vec1[j] + vec2[j],"\n")
-   }
   }
-```
+}
 
-#### Item c) Comprobamos en una matriz cuadrada en su diagonal si alguna de las cadenas de caractereseb la diagonal comienza con la letra g, minuscula o mayuscula 
+
+#### Item c) comprobamos en una matriz cuadrada en su diagonal si alguna de las cadenas de caractereseb la diagonal comienza con la letra g, minuscula o mayuscula 
 #### Por lo cual definimos una matriz mymat1, mymat2,mymat3
-
-```{r}
 
 mymat1 <- matrix(as.character(1:16),4,4)
 
@@ -55,10 +46,8 @@ mymat3 <- matrix(c("GREAT","ejercicios","agua","hey"),2,2,byrow = T)
 cat("\nmatriz mymat3 : \n")
 mymat3
 
-```
 #### Analizamos cada matriz
 #### Para la matriz mymat1
-```{r}
 
 dia_matriz <- diag(mymat1)
 estado <- FALSE 
@@ -72,23 +61,20 @@ for(i in 1:length(dia_matriz)){
 if(estado == FALSE){
   for(i in 1:length(dia_matriz)){
     for (j in 1:length(dia_matriz)) {
+      
+      if(i==j){
+        mymat1[i,j] <- 1
         
-           if(i==j){
-             mymat1[i,j] <- 1
-             
-           }else{
-             mymat1[i,j] <- 0
-           }
+      }else{
+        mymat1[i,j] <- 0
+      }
     }
     
   }
 }
 mymat1
-```
 
 #### Para la matriz mymat2
-
-```{r}
 dia_matriz <- diag(mymat2)
 estado <- FALSE 
 for(i in 1:length(dia_matriz)){
@@ -101,25 +87,20 @@ for(i in 1:length(dia_matriz)){
 if(estado == FALSE){
   for(i in 1:length(dia_matriz)){
     for (j in 1:length(dia_matriz)) {
+      
+      if(i==j){
+        mymat2[i,j] <- 1
         
-           if(i==j){
-             mymat2[i,j] <- 1
-             
-           }else{
-             mymat2[i,j] <- 0
-           }
+      }else{
+        mymat2[i,j] <- 0
+      }
     }
     
   }
 }
 mymat2
 
-```
-
-
 #### Para la matriz mymat3
-
-```{r}
 dia_matriz <- diag(mymat3)
 estado <- FALSE 
 for(i in 1:length(dia_matriz)){
@@ -132,20 +113,19 @@ for(i in 1:length(dia_matriz)){
 if(estado == FALSE){
   for(i in 1:length(dia_matriz)){
     for (j in 1:length(dia_matriz)) {
+      
+      if(i==j){
+        mymat3[i,j] <- 1
         
-           if(i==j){
-             mymat3[i,j] <- 1
-             
-           }else{
-             mymat3[i,j] <- 0
-           }
+      }else{
+        mymat3[i,j] <- 0
+      }
     }
     
   }
 }
 mymat3
 
-```
 
 
 
